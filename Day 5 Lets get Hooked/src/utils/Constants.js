04b9d52db26809c168b8a338b18d82abade2a791
aryangupta1105 <husltegamingfanclub@gmail.com>
2024-05-16 23:30:40 +0530

    updates

--- Day 5 Lets get Hooked/src/utils/Constants.js
+++
@@ -1,7 +0,0 @@
-export const CDN_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/'
-
-export const LOGO_URL = "https://png.pngtree.com/png-vector/20230217/ourmid/pngtree-food-logo-design-for-restaurant-and-business-png-image_6604922.png";
-
-export const CURR_YEAR = new Date().getFullYear();
-
-export const error_url = '../utils/error.gif';
